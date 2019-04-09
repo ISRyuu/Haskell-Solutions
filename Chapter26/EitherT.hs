4e5d040a96290900a8cca587a2ac0e1039c9f4fa
stardust <lql9705@gmail.com>
2019-04-09 19:06:26 +0800

    EitherT

--- Chapter26/EitherT.hs
+++ Chapter26/EitherT.hs
@@ -1,7 +1,7 @@
 import Control.Applicative
 
 newtype EitherT e m a =
-  EitherT {runEitherT :: m (Either e a)}
+  EitherT {runEitherT :: m (Either e a)} 
 
 instance Functor m => Functor (EitherT e m) where
   fmap f (EitherT mea) = EitherT $ (fmap . fmap) f mea
@@ -18,3 +18,17 @@ instance Monad m => Monad (EitherT e m) where
     case a of
       (Left x) -> return $ Left x
       (Right x) -> runEitherT (f x)
+
+swapEither :: Either e a -> Either a e
+swapEither (Left x) = Right x
+swapEither (Right x) = Left x
+
+swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e
+swapEitherT (EitherT x) = EitherT $ swapEither <$> x
+
+eitherT :: Monad m =>
+           (a -> m c)
+        -> (b -> m c)
+        -> EitherT a m b
+        -> m c
+eitherT f g (EitherT mab) = mab >>= either f g
