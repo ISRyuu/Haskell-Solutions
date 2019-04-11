7f98f5ca9f13b3aa69ec2e2dd985b03d1bbb89bd
stardust <lql9705@gmail.com>
2019-04-11 16:45:42 +0800

    StateT

---
+++ Chapter26/StateT.hs
@@ -0,0 +1,23 @@
+import Control.Applicative
+
+newtype StateT s m a =
+  StateT { runStateT :: s -> m (a, s) }
+
+instance Functor m => Functor (StateT s m) where
+  fmap f (StateT sma) = StateT $ \s ->
+    fmap (\(x, s) -> (f x, s)) $ sma s
+
+instance Monad m => Applicative (StateT s m) where
+  pure a = StateT $ \s -> pure (a, s)
+  (<*>) (StateT smab) (StateT sma) =
+    StateT $ \s -> do
+      (f, s') <- smab s
+      (a, s'') <- sma s'
+      return (f a, s'')
+
+instance Monad m => Monad (StateT s m) where
+  return = pure
+  (>>=) (StateT sma) f =
+    StateT $ \s -> do
+      (a, s') <- sma s
+      runStateT (f a) s'
